94b0809714bd92e5a534c69733bfe91adfbef388
殊怀丶 <lvzhihe_123@qq.com>
2021-03-31 16:24:53 +0800

    modified learn.py

--- Learn.py
+++ Learn.py
@@ -7,3 +7,31 @@ maxnumber = max(numbers)
 minnumber = min(numbers)
 del numbers[9]
 print(numbers)
+numbers.append(10)
+print(numbers)
+print(id(numbers))
+numbers.append(11)
+print(id(numbers))
+numbers += [12]
+print(id(numbers))
+print(id(numbers[0]))
+numbers[0] = -1
+print(id(numbers[0]))
+print(id(numbers))
+import time
+
+start = time.time()
+time.sleep(1)
+print(time.time() - start)
+x = list("fuck")
+print(x)
+x = [[10086] * 2] * 3
+print(x)
+x[0][0] = 1
+print(x)
+x = [1, 2, 1, 2, 1, 1, 1]
+for i in x[0:7:1]:
+    print(i)
+    if i == 1:
+        x.remove(i)
+        print(x)
