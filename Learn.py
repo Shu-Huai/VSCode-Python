a84af4b1fd4307858a49db43d26e47366193a856
殊怀 吕 <lvzhihe_123@qq.com>
2021-03-31 20:54:21 +0800

    modified Learn.py

--- Learn.py
+++ Learn.py
@@ -35,3 +35,24 @@ for i in x[0:7:1]:
     if i == 1:
         x.remove(i)
         print(x)
+try:
+    print(x[10])
+except IndexError:
+    print("No x[10].")
+try:
+    print(x.index(10))
+except ValueError:
+    print("No 10 in x.")
+print("2 number in x: ", x.count(2))
+print("1 number in x: ", x.count(1))
+print("3 in x: ", 3 in x)
+print("2 not in x: ", 2 not in x)
+del x
+List0 = [3, 5, 7, 9, 11]
+List1 = ['a', 'b', 'c']
+print("3, 'a' in zip: ", (3, 'a') in zip(List0, List1))
+for a, b in zip(List0, List1):
+    print(a, b)
+s1='programming'
+s2='language'
+print(s1.capitalize() + ' ' + s2.upper())
