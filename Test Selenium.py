9a3c5f4ca825a7b8a526590bdcc074a00e2abbd3
殊怀 吕 <lvzhihe_123@qq.com>
2021-04-19 23:14:21 +0800

    added Test Selenium.py

---
+++ Test Selenium.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+
+browser = webdriver.Edge(r'./.vscode/MicrosoftWebDriver.exe')
+browser.get(r"https://www.baidu.com")
+browser.find_element_by_id("kw").send_keys("selenium")
+browser.find_element_by_id("su").click()
+print(browser.page_source)
